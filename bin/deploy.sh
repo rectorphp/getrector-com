250615248ac381c63a1ae197d7d82c1ea1324118
TomasVotruba <tomas.vot@gmail.com>
2019-11-10 17:29:52 +0100

    more notes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,7 +9,7 @@ eval $(ssh-agent -s)
 mkdir -p ~/.ssh
 ssh-keyscan -H getrector.org >> ~/.ssh/known_hosts
 
-# create from private key with: https://www.samltool.com/format_privatekey.php
+# create single-line string from private key with https://www.samltool.com/format_privatekey.php
 # you need the private key from the remote server on your server (e.g. Digital Ocean)
 echo "$DEPLOY_PRIVATE_KEY" | ssh-add - > /dev/null
 ssh root@pehapkari.cz "cd /projects/getrector.org && ./run.sh"
