9df0347f45fb1911019ce1af302ea650e22d36c7
TomasVotruba <tomas.vot@gmail.com>
2019-11-10 20:46:50 +0100

    add link to private key issues

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,9 +8,9 @@ docker push rector/getrector.org
 eval $(ssh-agent -s)
 mkdir -p ~/.ssh
 # here is the name of the server we connect to, not final domain
-ssh-keyscan -H pehapkari.cz >> ~/.ssh/known_hosts
+ssh-keyscan -H getrector.org >> ~/.ssh/known_hosts
 
-# create single-line string from private key with https://www.samltool.com/format_privatekey.php
-# you need the private key from the remote server on your server (e.g. Digital Ocean)
+# create single-line string from private key from remote server on your server (e.g. Digital Ocean)
+# see https://github.com/travis-ci/travis-ci/issues/7715
 echo "$DEPLOY_PRIVATE_KEY" | ssh-add - > /dev/null
-ssh root@pehapkari.cz "cd /projects/getrector.org && ./run.sh"
+ssh root@getrector.org "cd /projects/getrector.org && ./run.sh"
