6d59efb9aa134dc1a4dade0a8529fe8a5bf701cb
Jan Mikeš <j.mikes@me.com>
2020-01-17 10:05:38 +0100

    Installed sentry/sentry-symfony composer package

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
