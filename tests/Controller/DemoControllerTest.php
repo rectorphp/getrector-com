073fe7c826384466a67b5c05170db2f16ed1ae61
TomasVotruba <tomas.vot@gmail.com>
2020-01-19 23:11:39 +0100

    add submit failed form test

--- tests/Controller/DemoControllerTest.php
+++ tests/Controller/DemoControllerTest.php
@@ -7,13 +7,33 @@ namespace Rector\Website\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * @see \Rector\Website\Form\DemoFormType
+ */
 final class DemoControllerTest extends WebTestCase
 {
     public function test(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+
+        // must be path of the controller
+        $client->request('GET', '/demo');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testFormSubmit(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/demo');
+
+        # use name="..." of the form
+        $client->submitForm('demo_form[process]', [
+            'demo_form[content]' => 'failed',
+            'demo_form[config]' => 'services:',
+        ]);
+
+        // no redirect, because PHP was invalid
+        $this->assertRouteSame('demo');
     }
 }
