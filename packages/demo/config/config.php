9b15c822e0eb2bc86ca554c9df9debae5b23bcbf
TomasVotruba <tomas.vot@gmail.com>
2020-07-31 02:30:54 +0200

    conver packages YAML to PHP

---
+++ packages/demo/config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Website\Demo\HttpKernel\Controller\RectorRunValueResolver;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Rector\Website\Demo\\', __DIR__ . '/../src/')
+        ->exclude(
+            [__DIR__ . '/../src/Exception/*', __DIR__ . '/../src/Entity/*', __DIR__ . '/../src/ValueObject/*', __DIR__ . '/../src/Validator/Constraint/*']
+        );
+
+    $services->set(RectorRunValueResolver::class)
+        ->tag('controller.argument_value_resolver', ['priority' => 550]);
+};
