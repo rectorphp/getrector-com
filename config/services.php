208c3d32011f912a2d7a5933fd4dc9b50f000158
Tomas Votruba <tomas.vot@gmail.com>
2020-11-25 00:38:20 +0000

    fixup! update to Symplify 9

--- config/services.php
+++ config/services.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 use Rector\Website\Demo\ValueObject\Option;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
 use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
+use Symplify\PackageBuilder\Reflection\PrivatesAccessor;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
