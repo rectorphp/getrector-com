327040a2db9446a790991df0f4452ef75f50e90f
TomasVotruba <tomas.vot@gmail.com>
2020-12-07 21:42:34 +0100

    add mission page

---
+++ src/Controller/MissionController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Website\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class MissionController extends AbstractController
+{
+    #[Route('mission', name: 'mission')]
+    public function __invoke(): Response
+    {
+        return $this->render('homepage/mission.twig');
+    }
+}
