fc375b92b6a43e9c2340ecda192520491603fa7f
TomasVotruba <tomas.vot@gmail.com>
2020-12-07 20:25:05 +0100

    simplify demo, add menu links

---
+++ packages/blog/src/Twig/RoutingTwigExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Website\Blog\Twig;
+
+use Rector\Website\Exception\ShouldNotHappenException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class RoutingTwigExtension extends AbstractExtension
+{
+    public function __construct(private RequestStack $requestStack)
+    {
+    }
+
+    /**
+     * @return TwigFunction[]
+     */
+    public function getFunctions(): iterable
+    {
+        $isCurrentRoute = new TwigFunction(
+            'is_current_route',
+            fn (string $desiredRouteName): bool => $this->isCurrentRoute($desiredRouteName)
+        );
+
+        $isCurrentRoutes = new TwigFunction('is_current_routes', function (array $desiredRouteNames): bool {
+            foreach ($desiredRouteNames as $desiredRouteName) {
+                if (! $this->isCurrentRoute($desiredRouteName)) {
+                    continue;
+                }
+
+                return true;
+            }
+
+            return false;
+        });
+
+        return [$isCurrentRoute, $isCurrentRoutes];
+    }
+
+    private function isCurrentRoute(string $desiredRouteName): bool
+    {
+        $currentRequest = $this->requestStack->getCurrentRequest();
+        if ($currentRequest === null) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->resolveCurrentRoute($currentRequest) === $desiredRouteName;
+    }
+
+    private function resolveCurrentRoute(Request $request): string
+    {
+        $currentRouteName = $request->get('_route');
+
+        return ltrim($currentRouteName, '/');
+    }
+}
