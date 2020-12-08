0dfd8182cef07850ce7e71d9b4a94efc44aad2b2
TomasVotruba <tomas.vot@gmail.com>
2020-12-08 21:39:10 +0100

    drop doctrine migratiosn

--- config/bundles.php
+++ config/bundles.php
@@ -8,10 +8,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
-    // for some reason symfony can't live without this bundle
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-
-    // dev only
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
