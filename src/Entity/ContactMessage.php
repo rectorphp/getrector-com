f5a70d53354cc3f0f1b79a2105fbd092172b5fe5
Tomas Votruba <tomas.vot@gmail.com>
2020-12-14 12:43:05 +0100

    ContactMessage - add is replied bool (#214)

--- src/Entity/ContactMessage.php
+++ src/Entity/ContactMessage.php
@@ -23,39 +23,134 @@ class ContactMessage implements TimestampableInterface
      * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class=UuidV4Generator::class)
      */
-    public Uuid $id;
+    private Uuid $id;
     /**
      * @ORM\Column(type="text")
      */
-    public string $message;
+    private string $message;
 
     /**
      * @ORM\Column(type="string")
      */
-    public string $name;
+    private string $name;
 
     /**
      * @ORM\Column(type="string")
      */
-    public string $email;
+    private string $email;
 
     /**
      * @ORM\Column(type="integer")
      */
-    public int $projectSize;
+    private int $projectSize;
 
     /**
      * @ORM\Column(type="string")
      */
-    public string $framework;
+    private string $framework;
 
     /**
      * @ORM\Column(type="integer")
      */
-    public int $currentPhpVersion;
+    private int $currentPhpVersion;
 
     /**
      * @ORM\Column(type="integer", nullable=true)
      */
-    public ?int $targetPhpVersion = null;
+    private ?int $targetPhpVersion = null;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $isReplied = false;
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getProjectSize(): int
+    {
+        return $this->projectSize;
+    }
+
+    public function setProjectSize(int $projectSize): void
+    {
+        $this->projectSize = $projectSize;
+    }
+
+    public function getFramework(): string
+    {
+        return $this->framework;
+    }
+
+    public function setFramework(string $framework): void
+    {
+        $this->framework = $framework;
+    }
+
+    public function getCurrentPhpVersion(): int
+    {
+        return $this->currentPhpVersion;
+    }
+
+    public function setCurrentPhpVersion(int $currentPhpVersion): void
+    {
+        $this->currentPhpVersion = $currentPhpVersion;
+    }
+
+    public function getTargetPhpVersion(): ?int
+    {
+        return $this->targetPhpVersion;
+    }
+
+    public function setTargetPhpVersion(?int $targetPhpVersion): void
+    {
+        $this->targetPhpVersion = $targetPhpVersion;
+    }
+
+    public function isReplied(): bool
+    {
+        return $this->isReplied;
+    }
+
+    public function setIsReplied(bool $isReplied): void
+    {
+        $this->isReplied = $isReplied;
+    }
 }
