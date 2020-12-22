f09e9026e6c3b1c84b614a7acd8c97e73eae9a55
TomasVotruba <tomas.vot@gmail.com>
2020-12-22 17:41:20 +0100

    improve demo-config.php

--- packages/demo/data/demo-config.php
+++ packages/demo/data/demo-config.php
@@ -5,11 +5,17 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\Generic\Tests\Rector\Property\AnnotatedPropertyInjectToConstructorInjectionRector\FixtureTypedProperty\TypedProperty;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
+    $services = $containerConfigurator->services();
 
+    // A. run whole set
     $parameters->set(Option::SETS, [SetList::DEAD_CODE]);
+
+    // B. or single rule
+    // $services->set(TypedProperty::class);
 };
