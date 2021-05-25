e8f8328de2f14df6e81a2eca149bd756b654b886
TomasVotruba <tomas.vot@gmail.com>
2021-05-25 17:15:11 +0100

    add simple captcha, simplify contact form to one page

--- src/Controller/ForCompaniesController.php
+++ src/Controller/ForCompaniesController.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace Rector\Website\Controller;
 
-use Rector\Website\Form\ContactFormType;
-use Rector\Website\FormProcessor\ContactFormProcessor;
 use Rector\Website\Twig\ResponseRenderer;
 use Rector\Website\ValueObject\Routing\RouteName;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,26 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 final class ForCompaniesController
 {
     public function __construct(
-        private ContactFormProcessor $contactFormProcessor,
         private ResponseRenderer $responseRenderer,
-        private FormFactoryInterface $formFactory
     ) {
     }
 
     #[Route(path: 'for-companies', name: RouteName::FOR_COMPANIES)]
     public function __invoke(Request $request): Response
     {
-        $form = $this->formFactory->create(ContactFormType::class, null, [
-            'for_companies' => true,
-        ]);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            return $this->contactFormProcessor->process($form, RouteName::FOR_COMPANIES);
-        }
-
         return $this->responseRenderer->render('homepage/for_companies.twig', [
-            'contact_form' => $form->createView(),
             'title' => 'Do you have a Successful Project, but old Code Programmers Hate',
         ]);
     }
