49087fda6de5e90a2e90c9be6116a56ab4f33ebf
Tomas Votruba <tomas.vot@gmail.com>
2021-12-17 16:03:55 +0100

    fix session factory name

--- config/packages/test/framework.php
+++ config/packages/test/framework.php
@@ -8,7 +8,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('framework', [
         'test' => null,
         'session' => [
-            'storage_factory_id' => 'session.storage.mock_file',
+            'storage_factory_id' => 'session.storage.factory.mock_file',
         ],
     ]);
 };
