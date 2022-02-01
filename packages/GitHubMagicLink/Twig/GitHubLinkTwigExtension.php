d88aa9ac72b9c335388fa172f72342f980b498a6
Tomas Votruba <tomas.vot@gmail.com>
2022-02-01 22:57:00 +0100

    merge twig link extension to one

---
+++ packages/GitHubMagicLink/Twig/GitHubLinkTwigExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Website\GitHubMagicLink\Twig;
+
+use Rector\Website\Demo\Entity\RectorRun;
+use Rector\Website\GitHubMagicLink\LinkFactory\FixtureLinkFactory;
+use Rector\Website\GitHubMagicLink\LinkFactory\IssueLinkFactory;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class GitHubLinkTwigExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly FixtureLinkFactory $fixtureLinkFactory,
+        private readonly IssueLinkFactory $issueLinkFactory,
+    ) {
+    }
+
+    /**
+     * @return TwigFilter[]
+     */
+    public function getFilters(): array
+    {
+        $prLinkTwigFilter = new TwigFilter('pr_link', fn (RectorRun $rectorRun): string => $this->fixtureLinkFactory->create(
+            $rectorRun
+        ));
+
+        $issueLinkTwigFilter = new TwigFilter('issue_link', fn (RectorRun $rectorRun): string => $this->issueLinkFactory->create(
+            $rectorRun
+        ));
+
+        return [$prLinkTwigFilter, $issueLinkTwigFilter];
+    }
+}
