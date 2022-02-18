b8148fc7e426b2a5ec5f8d55203012f5ccfbd90b
Tomas Votruba <tomas.vot@gmail.com>
2022-02-18 14:06:22 +0000

    enable sessions

--- config/packages/framework.php
+++ config/packages/framework.php
@@ -9,6 +9,7 @@ use Symplify\Amnesia\ValueObject\Symfony\Extension\FrameworkExtension;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension(FrameworkExtension::NAME, [
         FrameworkExtension::SECRET => env('APP_SECRET'),
+        FrameworkExtension::SESSION => true,
         FrameworkExtension::HTTP_METHOD_OVERRIDE => true,
         FrameworkExtension::TRUSTED_HOSTS => null,
         FrameworkExtension::FRAGMENTS => [
