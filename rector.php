f45f34074f1503012054fd07641d015d9318f49d
Tomas Votruba <tomas.vot@gmail.com>
2022-04-12 10:55:22 +0200

    bump rector to latest dev-main, use RectorConfig

--- rector.php
+++ rector.php
@@ -2,43 +2,42 @@
 
 declare(strict_types=1);
 
-use PHPUnit\Framework\TestCase;
 use Rector\CodingStyle\Enum\PreferenceSelfThis;
 use Rector\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector;
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/packages']);
+
+    $rectorConfig->autoImportNames();
+
+    $rectorConfig->parallel();
+
+    $rectorConfig->sets([
+        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::NAMING,
+        SetList::TYPE_DECLARATION,
+    ]);
+
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
+
+    $rectorConfig->skip(['*/var/cache/*', __DIR__ . '/packages/Demo/data/DemoFile.php']);
+
+    $services = $rectorConfig->services();
     $services->set(FinalizeClassesWithoutChildrenRector::class);
 
-    $services->set(PreferThisOrSelfMethodCallRector::class)
-        ->configure([
-            TestCase::class => PreferenceSelfThis::PREFER_THIS(),
-        ]);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/packages']);
-
-    $containerConfigurator->import(SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $containerConfigurator->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::CODING_STYLE);
-    $containerConfigurator->import(SetList::NAMING);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
-
-    $parameters->set(Option::SKIP, ['*/var/cache/*', __DIR__ . '/packages/Demo/data/DemoFile.php']);
+    $rectorConfig->ruleWithConfiguration(PreferThisOrSelfMethodCallRector::class, [
+        'PHPUnit\Framework\TestCase' => PreferenceSelfThis::PREFER_THIS(),
+    ]);
 };
