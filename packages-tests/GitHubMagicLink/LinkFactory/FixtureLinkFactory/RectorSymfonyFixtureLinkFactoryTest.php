0cfcce56ab36686771c5ab04131ad65be012db5e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-24 19:44:26 +0700

    [GithubMagicLink] Handle Create a Test link on rector-* packages

---
+++ packages-tests/GitHubMagicLink/LinkFactory/FixtureLinkFactory/RectorSymfonyFixtureLinkFactoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Website\Tests\GitHubMagicLink\LinkFactory\FixtureLinkFactory;
+
+use Rector\Website\Tests\Demo\Helpers\DummyRectorRunFactory;
+use Rector\Website\GetRectorKernel;
+use Rector\Website\GitHubMagicLink\LinkFactory\FixtureLinkFactory;
+use Rector\Website\Tests\Demo\Helpers\StringToArrayRunFactory;
+use Symplify\PackageBuilder\Testing\AbstractKernelTestCase;
+
+final class RectorSymfonyFixtureLinkFactoryTest extends AbstractKernelTestCase
+{
+    private FixtureLinkFactory $testFixtureLinkFactory;
+
+    private StringToArrayRunFactory $stringToArrayRunFactory;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(GetRectorKernel::class);
+        $this->testFixtureLinkFactory = $this->getService(FixtureLinkFactory::class);
+        $this->stringToArrayRunFactory = new StringToArrayRunFactory();
+    }
+
+    public function test(): void
+    {
+        $rectorRun = $this->stringToArrayRunFactory->create();
+        $testFixtureLink = $this->testFixtureLinkFactory->create($rectorRun);
+
+        $this->assertStringMatchesFormatFile(__DIR__ . '/FixtureRectorSymfony/expected_link.txt', $testFixtureLink . PHP_EOL);
+    }
+}
