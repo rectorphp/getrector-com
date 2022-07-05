759d80ccf516c85bbb31e861ca6df276fe5db288
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-07-05 17:29:18 +0700

    Fix amnesia on routes.php config

--- config/routes.php
+++ config/routes.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-use Symplify\Amnesia\ValueObject\Symfony\Routing;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
-    $routingConfigurator->import(__DIR__ . '/../src/Controller', Routing::TYPE_ANNOTATION);
-    $routingConfigurator->import(__DIR__ . '/../packages/Blog/Controller', Routing::TYPE_ANNOTATION);
-    $routingConfigurator->import(__DIR__ . '/../packages/Demo/Controller', Routing::TYPE_ANNOTATION);
+    $routingConfigurator->import(__DIR__ . '/../src/Controller', 'annotation');
+    $routingConfigurator->import(__DIR__ . '/../packages/Blog/Controller', 'annotation');
+    $routingConfigurator->import(__DIR__ . '/../packages/Demo/Controller', 'annotation');
 };
