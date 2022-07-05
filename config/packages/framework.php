baa977e9fdac270984afb893fa24065628fa8811
Tomas Votruba <tomas.vot@gmail.com>
2022-07-05 12:13:10 +0200

    remove amnesia classes

--- config/packages/framework.php
+++ config/packages/framework.php
@@ -3,21 +3,10 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use function Symplify\Amnesia\Functions\env;
-use Symplify\Amnesia\ValueObject\Symfony\Extension\FrameworkExtension;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension(FrameworkExtension::NAME, [
-        FrameworkExtension::SECRET => env('APP_SECRET'),
-        FrameworkExtension::SESSION => true,
-        FrameworkExtension::HTTP_METHOD_OVERRIDE => true,
-        FrameworkExtension::TRUSTED_HOSTS => null,
-        FrameworkExtension::FRAGMENTS => [
-            'enabled' => true,
-        ],
-        FrameworkExtension::PHP_ERRORS => [
-            'log' => true,
-        ],
-        FrameworkExtension::IDE => 'phpstorm',
+    $containerConfigurator->extension('framework', [
+        'secret' => '%env(APP_SECRET)',
+        'session' => true,
     ]);
 };
--- config/packages/twig.php
+++ config/packages/twig.php
@@ -3,28 +3,25 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\Amnesia\ValueObject\Symfony\Extension\Twig\NumberFormat;
-use Symplify\Amnesia\ValueObject\Symfony\Extension\TwigExtension;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension(TwigExtension::NAME, [
-        TwigExtension::FORM_THEMES => ['bootstrap_4_layout.html.twig'],
-        TwigExtension::DEFAULT_PATH => '%kernel.project_dir%/templates',
-        TwigExtension::DEBUG => '%kernel.debug%',
-        TwigExtension::STRICT_VARIABLES => true,
-        TwigExtension::EXCEPTION_CONTROLLER => null,
-        TwigExtension::GLOBALS => [
+    $containerConfigurator->extension('twig', [
+        'form_themes' => ['bootstrap_4_layout.html.twig'],
+        'default_path' => '%kernel.project_dir%/templates',
+        'debug' => '%kernel.debug%',
+        'strict_variables' => true,
+        'globals' => [
             'site_url' => 'https://getrector.org',
             'main_page_title' => 'Rector - Automated Way to Instantly Upgrade and Refactor any PHP code',
             'disqus_name' => 'getrectororg',
         ],
-        TwigExtension::DATE => [
+        'date' => [
             'format' => 'F d, Y',
         ],
-        TwigExtension::NUMBER_FORMAT => [
-            NumberFormat::DECIMALS => 0,
-            NumberFormat::DECIMAL_POINT => ',',
-            NumberFormat::THOUSANDS_SEPARATOR => ' ',
+        'number_format' => [
+            'decimals' => 0,
+            'decimal_point' => ',',
+            'thousands_separator' => ' ',
         ],
     ]);
 };
