709a5f09562142f1dd04d60acfb9250eef96d5ee
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-20 20:42:23 +0700

    use only 6 chars hash

--- packages/Demo/Controller/DemoController.php
+++ packages/Demo/Controller/DemoController.php
@@ -72,11 +72,19 @@ final class DemoController extends AbstractController
         }
 
         return $this->render('demo/demo.twig', [
-            'rector_version' => Versions::getVersion('rector/rector'),
+            'rector_version' => $this->resolveRectorReleaseVersion(),
             'rector_released_time' => VersionResolver::RELEASE_DATE,
             'demo_form' => $demoForm->createView(),
             'rector_run' => $rectorRun,
         ]);
+    }
+
+    private function resolveRectorReleaseVersion(): string
+    {
+        $rectorVersion = Versions::getVersion('rector/rector');
+        $extractAt = explode('@', $rectorVersion);
+
+        return $extractAt[0] . '@' . substr($extractAt[1], 0, 6);
     }
 
     private function processFormAndReturnRoute(RectorRun $rectorRun): RedirectResponse
