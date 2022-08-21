a4414133f5974b22f97773ced398198ddbcf6c02
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-08-21 21:22:03 +0700

    [Test] Add Acceptance test to avoid unknown error 500 (#723)
    * [Test] Add Acceptance test to avoid unknown error 500
    
    * workflow name
    
    * fix
    
    * fix
    
    * eol
    
    * show diff on demo page
    
    * name
    
    * final touch

---
+++ tests/Acceptance/DemoPageCest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class DemoPageCest
+{
+    public function showDemoPage(AcceptanceTester $I)
+    {
+        $I->amOnPage('/demo');
+
+        // see text in demo page
+        $I->see('Rector version: dev-main@');
+    }
+
+    public function clickProcessButton(AcceptanceTester $I)
+    {
+        $I->amOnPage('/demo');
+        $I->click('#demo_form_process');
+
+        // see text in demo page after process button clicked
+        $I->see('What did Rector change?');
+        $I->see('-        // we never get here');
+    }
+}
