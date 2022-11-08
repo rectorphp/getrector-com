77ebb48e7fe397ca59e82dbaa2849da67f8f8e58
Tomas Votruba <tomas.vot@gmail.com>
2022-11-08 17:58:39 +0100

    rector apply

--- packages/Blog/ValueObjectFactory/PostFactory.php
+++ packages/Blog/ValueObjectFactory/PostFactory.php
@@ -89,11 +89,7 @@ final class PostFactory
         ) : null;
         $deprecatedMessage = $configuration['deprecated_message'] ?? null;
 
-        if (isset($configuration['since_rector'])) {
-            $sinceRector = (string) $configuration['since_rector'];
-        } else {
-            $sinceRector = null;
-        }
+        $sinceRector = isset($configuration['since_rector']) ? (string) $configuration['since_rector'] : null;
 
         return new Post(
             $id,
@@ -122,11 +118,10 @@ final class PostFactory
      */
     private function decorateHeadlineWithId(string $htmlContent): string
     {
-        return Strings::replace($htmlContent, self::HEADLINE_REGEX, function ($matches): string {
+        return Strings::replace($htmlContent, self::HEADLINE_REGEX, static function ($matches) : string {
             $level = $matches['level'];
             $headline = $matches['headline'];
             $idValue = Strings::webalize($headline);
-
             return sprintf('<h%d id="%s">%s</h%d>', $level, $idValue, $headline, $level);
         });
     }
