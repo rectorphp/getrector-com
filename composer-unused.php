2a469f0049e563d4d0671f20d894d8be51e00b6d
Tomas Votruba <tomas.vot@gmail.com>
2022-11-29 01:38:24 +0100

    notes

--- composer-unused.php
+++ composer-unused.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
 
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
 use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
-use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
 
 return static function (Configuration $config): Configuration {
     // rector dependency for running "online demo"
