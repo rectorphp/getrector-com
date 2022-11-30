2e0e066196008f2472933a04c57b10410df1b478
Tomas Votruba <tomas.vot@gmail.com>
2022-11-30 13:17:16 +0100

    extract factory

--- src/Controller/DocsSectionController.php
+++ src/Controller/DocsSectionController.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 namespace Rector\Website\Controller;
 
-use Nette\Utils\FileSystem;
-use ParsedownExtra;
 use Rector\Website\Documentation\DocumentationMenuFactory;
+use Rector\Website\Documentation\HTMLFromMarkdownFactory;
 use Rector\Website\ValueObject\Routing\RouteName;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Webmozart\Assert\Assert;
 
 final class DocsSectionController extends AbstractController
 {
     public function __construct(
-        private readonly ParsedownExtra $parsedownExtra,
+        private readonly HTMLFromMarkdownFactory $htmlFromMarkdownFactory,
         private readonly DocumentationMenuFactory $documentationMenuFactory
     ) {
     }
@@ -25,7 +23,7 @@ final class DocsSectionController extends AbstractController
     public function __invoke(string $section): Response
     {
         $sectionMarkdownFilePath = __DIR__ . '/../../data/docs/' . $section . '.md';
-        $sectionHtmlContents = $this->createHtmlSectionContents($sectionMarkdownFilePath);
+        $sectionHtmlContents = $this->htmlFromMarkdownFactory->create($sectionMarkdownFilePath);
 
         // render menu ;)
         return $this->render('docs/section.twig', [
@@ -33,13 +31,5 @@ final class DocsSectionController extends AbstractController
             'section_html_contents' => $sectionHtmlContents,
             'documentations_sections' => $this->documentationMenuFactory->create(),
         ]);
-    }
-
-    private function createHtmlSectionContents(string $markdownFilePath): string
-    {
-        Assert::fileExists($markdownFilePath);
-        $sectionFileContents = FileSystem::read($markdownFilePath);
-
-        return $this->parsedownExtra->parse($sectionFileContents);
     }
 }
