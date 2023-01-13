0d8b741467678c1f397b4e388bc82cc39e961cef
Tomas Votruba <tomas.vot@gmail.com>
2023-01-13 09:38:02 +0100

    note

--- tests/Acceptance/SupportPageCest.php
+++ tests/Acceptance/SupportPageCest.php
@@ -14,6 +14,6 @@ final class SupportPageCest
         $acceptanceTester->amOnPage('/for-companies');
 
         // see text in for-companies page
-        $acceptanceTester->see('Hire Rector team to Reduce Costs and Technical Debt?');
+        $acceptanceTester->see('Hire Rector team to Reduce Costs and Technical Debt');
     }
 }
