0f480d3c19625193e7d8e06105667b8f7e76da2d
Tomas Votruba <tomas.vot@gmail.com>
2023-01-15 20:01:35 +0000

    tv no em css (#1004)

---
+++ bin/only-rem-in-css.php
@@ -0,0 +1,29 @@
+<?php
+
+use Nette\Utils\FileSystem;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$cssFilePaths = [
+    __DIR__ . '/../assets/scss/app.scss',
+    __DIR__ . '/../assets/scss/menu.scss',
+];
+
+$exitCode = \Symfony\Component\Console\Command\Command::SUCCESS;
+
+foreach ($cssFilePaths as $cssFilePath) {
+    $cssFileContents = FileSystem::read($cssFilePath);
+
+    $emMatches = \Nette\Utils\Strings::matchAll($cssFileContents, '#\dem\b#');
+
+    if ($emMatches !== []) {
+        echo sprintf('Found %d "em" uses in "%s" file', count($emMatches), realpath($cssFilePath)) . PHP_EOL;
+
+        // lower only in case of good mood :)
+        if (count($emMatches) > 85) {
+            $exitCode = \Symfony\Component\Console\Command\Command::FAILURE;
+        }
+    }
+}
+
+exit($exitCode);
