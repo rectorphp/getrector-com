564bea0a3cb9fa0f6689a990d62c144df986c7d6
Tomas Votruba <tomas.vot@gmail.com>
2023-02-07 19:36:13 +0000

    Add first Pest test (#1055)

--- tests/Documentation/DocumentationMenuFactoryTest.php
+++ tests/Documentation/DocumentationMenuFactoryTest.php
@@ -2,34 +2,25 @@
 
 declare(strict_types=1);
 
-namespace Rector\Website\Tests\Documentation;
-
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 use Rector\Website\Documentation\DocumentationMenuFactory;
 use Rector\Website\ValueObject\DocumentationSection;
 
-final class DocumentationMenuFactoryTest extends TestCase
-{
-    private DocumentationMenuFactory $documentationMenuFactory;
+test('section title', function () {
+    $documentationMenuFactory = new DocumentationMenuFactory();
 
-    protected function setUp(): void
-    {
-        $this->documentationMenuFactory = new DocumentationMenuFactory();
+    $sectionTitle = $documentationMenuFactory->createSectionTitle('run-in-cI');
+    expect($sectionTitle)
+        ->toBe('Run in CI');
+});
+
+test('create', function () {
+    $documentationMenuFactory = new DocumentationMenuFactory();
+
+    $documentationSectionsByCategory = $documentationMenuFactory->create();
+
+    foreach ($documentationSectionsByCategory as $category => $documentationSections) {
+        expect($category)->toBeString();
+        Assert::assertContainsOnlyInstancesOf(DocumentationSection::class, $documentationSections);
     }
-
-    public function testSectionTitle(): void
-    {
-        $sectionTitle = $this->documentationMenuFactory->createSectionTitle('run-in-cI');
-        $this->assertSame('Run in CI', $sectionTitle);
-    }
-
-    public function testCreate(): void
-    {
-        $documentationSectionsByCategory = $this->documentationMenuFactory->create();
-
-        foreach ($documentationSectionsByCategory as $category => $documentationSections) {
-            $this->assertIsString($category);
-            $this->assertContainsOnlyInstancesOf(DocumentationSection::class, $documentationSections);
-        }
-    }
-}
+});
