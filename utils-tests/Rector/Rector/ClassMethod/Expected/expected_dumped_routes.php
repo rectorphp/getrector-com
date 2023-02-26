2f6ee4e85d3d3ef5ccae691852f9432a10a467b7
Tomas Votruba <tomas.vot@gmail.com>
2023-02-26 11:02:27 +0000

    fixup! misc

--- utils-tests/Rector/Rector/ClassMethod/Expected/expected_dumped_routes.php
+++ utils-tests/Rector/Rector/ClassMethod/Expected/expected_dumped_routes.php
@@ -1,8 +1,3 @@
 <?php
 
-declare(strict_types=1);
-
-\Illuminate\Support\Facades\Route::get(
-    '/some',
-    \TomasVotruba\Utils\Tests\Rector\Rector\ClassMethod\Fixture\SomeController::class
-)->name('some');
+\Illuminate\Support\Facades\Route::get('/some', \TomasVotruba\Utils\Tests\Rector\Rector\ClassMethod\Fixture\SomeController::class)->name('some');
