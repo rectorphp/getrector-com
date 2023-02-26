a802522fe7631f9d758569cb29d43c29055b4501
Tomas Votruba <tomas.vot@gmail.com>
2023-02-26 02:29:59 +0000

    dump routes

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,27 @@
 
 \Illuminate\Support\Facades\Route::get('rss.xml', \Rector\Website\Controller\RssController::class)->name('rss');
 
+\Illuminate\Support\Facades\Route::get('about', \Rector\Website\Controller\AboutController::class)->name('about');
+
+\Illuminate\Support\Facades\Route::get('blog', \Rector\Website\Controller\BlogController::class)->name('blog');
+
+\Illuminate\Support\Facades\Route::get('book', \Rector\Website\Controller\BookController::class)->name('book');
+
+\Illuminate\Support\Facades\Route::get('contact', \Rector\Website\Controller\ContactController::class)->name('contact');
+
+\Illuminate\Support\Facades\Route::get('demo/{uuid}', \Rector\Website\Controller\DemoController::class)->name('demo_detail');
+
+\Illuminate\Support\Facades\Route::get('demo', \Rector\Website\Controller\DemoController::class)->name('demo');
+
+\Illuminate\Support\Facades\Route::get('documentation/{section}', \Rector\Website\Controller\DocumentationController::class)->name('documentation');
+
+\Illuminate\Support\Facades\Route::get('for-companies', \Rector\Website\Controller\ForCompaniesController::class)->name('for_companies');
+
+\Illuminate\Support\Facades\Route::get('hire-team', \Rector\Website\Controller\ForCompaniesController::class)->name('hire_team');
+
+\Illuminate\Support\Facades\Route::get('/', \Rector\Website\Controller\HomepageController::class)->name('homepage');
+
+\Illuminate\Support\Facades\Route::get('blog/{postSlug}', \Rector\Website\Controller\PostController::class)->name('post');
+
+\Illuminate\Support\Facades\Route::get('rss.xml', \Rector\Website\Controller\RssController::class)->name('rss');
+
