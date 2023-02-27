e0bc0811474a08a94a1947c945a0c5f6e8de383e
Tomas Votruba <tomas.vot@gmail.com>
2023-02-27 23:16:49 +0000

    autoload fcs

--- src/Blade/functions.php
+++ src/Blade/functions.php
@@ -6,13 +6,6 @@ use Rector\Website\Entity\RectorRun;
 use Rector\Website\GitHubMagicLink\LinkFactory\FixtureLinkFactory;
 use Rector\Website\GitHubMagicLink\LinkFactory\IssueLinkFactory;
 
-function clearTitle(string $title): string
-{
-    $clearTitle = strip_tags($title);
-    return str_replace('&nbsp;', ' ', $clearTitle);
-}
-
-
 function prLink(RectorRun $rectorRun): string
 {
     /** @var FixtureLinkFactory $fixtureLinkFactory */
