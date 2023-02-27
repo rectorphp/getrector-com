6b12d37b1c94b10dace2a76e4ae403e6b62b08a2
Tomas Votruba <tomas.vot@gmail.com>
2023-02-27 22:11:55 +0000

    misc

--- bin/only-rem-in-css.php
+++ bin/only-rem-in-css.php
@@ -5,8 +5,8 @@ use Nette\Utils\FileSystem;
 require __DIR__ . '/../vendor/autoload.php';
 
 $cssFilePaths = [
-    __DIR__ . '/../assets/scss/app.scss',
-    __DIR__ . '/../assets/scss/menu.scss',
+    __DIR__ . '/../resources/css/app.scss',
+    __DIR__ . '/../resources/css/menu.scss',
 ];
 
 $exitCode = \Symfony\Component\Console\Command\Command::SUCCESS;
