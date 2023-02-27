dea14d7d107e33c97023419a198114c66f001721
Tomas Votruba <tomas.vot@gmail.com>
2023-02-27 22:14:16 +0000

    fix active classes

---
+++ easy-ci.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+return static function (\Symplify\EasyCI\Config\EasyCIConfig $easyCIConfig): void {
+    $easyCIConfig->paths([__DIR__ . '/app', __DIR__ . '/src', __DIR__ . '/config']);
+
+    $easyCIConfig->typesToSkip([
+
+        \Illuminate\Routing\Controller::class,
+        \Illuminate\Support\ServiceProvider::class,
+        \Illuminate\Contracts\Console\Kernel::class,
+        \Illuminate\Contracts\Http\Kernel::class,
+    ]);
+};
