94a72400304c02c649f61897faf9d41f43952b17
Tomas Votruba <tomas.vot@gmail.com>
2023-02-27 00:08:38 +0000

    add view config for Laravel

---
+++ config/view.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TomasVotruba\PunchCard\ViewConfig;
+
+return ViewConfig::make()
+    ->paths([
+        __DIR__ . '/../resources/views'
+    ])
+    ->compiled(__DIR__ . '/../storage/framework/views')
+    ->toArray();
