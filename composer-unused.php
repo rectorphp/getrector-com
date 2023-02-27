df2a674b4ba028bbf9de4521962fceed469cf1b4
Tomas Votruba <tomas.vot@gmail.com>
2023-02-27 22:07:15 +0000

    tidy

--- composer-unused.php
+++ composer-unused.php
@@ -17,16 +17,6 @@ return static function (Configuration $config): Configuration {
     // used in lot of code
     $config->addNamedFilter(NamedFilter::fromString('symplify/package-builder'));
 
-    // false positive
-    $config->addNamedFilter(NamedFilter::fromString('symfony/asset'));
-
-    // used in bootstrap.php to load .env file and set basic symfony env + debug state
-    $config->addNamedFilter(NamedFilter::fromString('symfony/dotenv'));
-
-    // used in bundles.php to twig-related configuration for Symfony
-    $config->addNamedFilter(NamedFilter::fromString('symfony/twig-bundle'));
-    $config->addNamedFilter(NamedFilter::fromString('symfony/webpack-encore-bundle'));
-
     // soon to be used in laravel project
     $config->addNamedFilter(NamedFilter::fromString('imagine/imagine'));
     $config->addNamedFilter(NamedFilter::fromString('spatie/laravel-markdown'));
