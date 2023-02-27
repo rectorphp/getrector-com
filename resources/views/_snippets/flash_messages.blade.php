4888e9300970ceb0962a0ee664cc19236cba7172
Tomas Votruba <tomas.vot@gmail.com>
2023-02-27 09:57:35 +0000

    Add flashes (#1120)

--- resources/views/_snippets/flash_messages.blade.php
+++ resources/views/_snippets/flash_messages.blade.php
@@ -1,3 +1,22 @@
+{{-- colorful flashes, see https://stackoverflow.com/a/47363646/1348344 --}}
+
+@foreach ([
+    \Rector\Website\Enum\FlashType::ERROR,
+    \Rector\Website\Enum\FlashType::WARNING,
+    \Rector\Website\Enum\FlashType::SUCCESS
+] as $flashMessageType)
+    @if(session()->has($flashMessageType))
+        <div class="container mt-4">
+            <p class="alert alert-{{ $flashMessageType }}">
+                {!! session()->get($flashMessageType) !!}
+            </p>
+        </div>
+    @endif
+
+@endforeach
+
+
+
 @foreach ($app.flashes as $type, messages)
     @foreach ($messages as $message)
         <div class="alert alert-{{ $type }} mt-4" role="alert">
