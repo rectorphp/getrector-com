59a5e7a30a8f48d95dc0e1dfce2021fdcfd40090
Tomas Votruba <tomas.vot@gmail.com>
2023-02-27 09:53:36 +0000

    add flashes

--- src/Enum/FlashType.php
+++ src/Enum/FlashType.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace Rector\Website\Enum;
 
+/**
+ * @api
+ */
 final class FlashType
 {
     /**
      * @var string
      */
     public const ERROR = 'danger';
+
+    /**
+     * @var string
+     */
+    public const SUCCESS = 'success';
+
+    /**
+     * @var string
+     */
+    public const WARNING = 'warning';
 }
