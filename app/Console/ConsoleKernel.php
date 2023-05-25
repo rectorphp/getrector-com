27ac0a36ce218432a596ec3f5295dc4e62b7eb81
Tomas Votruba <tomas.vot@gmail.com>
2023-05-25 09:06:15 +0200

    Bump deps + cleanup Laravelize (#1348)

--- app/Console/ConsoleKernel.php
+++ app/Console/ConsoleKernel.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace App\Console;
 
-use Illuminate\Console\Command;
 use Illuminate\Foundation\Console\Kernel;
-use Rector\Website\Utils\Laravelize\Command\TwigToBladeCommand;
 
 final class ConsoleKernel extends Kernel
 {
-    /**
-     * @var array<class-string<Command>>
-     */
-    protected $commands = [TwigToBladeCommand::class];
 }
