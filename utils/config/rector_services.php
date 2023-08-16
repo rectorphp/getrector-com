d690446292e506676e0143e8d813a7f6cf50d93b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-16 07:20:18 +0700

    Update rector_services.php

--- utils/config/rector_services.php
+++ utils/config/rector_services.php
@@ -7,10 +7,8 @@ use Rector\Website\Utils\Rector\NodeFactory\RouteGetCallFactory;
 use Rector\Website\Utils\Tests\Rector\NodeFactory\SignaturePropertyFactory;
 
 return static function (RectorConfig $rectorConfig): void {
-    $services = $rectorConfig->services();
-    $services->defaults()
-        ->autowire();
-
-    $services->set(RouteGetCallFactory::class);
-    $services->set(SignaturePropertyFactory::class);
+    $services->rules([
+        RouteGetCallFactory::class,
+        SignaturePropertyFactory::class,
+    ]);
 };
