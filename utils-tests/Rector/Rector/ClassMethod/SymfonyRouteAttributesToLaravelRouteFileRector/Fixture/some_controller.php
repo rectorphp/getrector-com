c5d1e88312630581f47bdd7398558735866a0290
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-08-24 19:15:27 +0700

    Fix

--- utils-tests/Rector/Rector/ClassMethod/SymfonyRouteAttributesToLaravelRouteFileRector/Fixture/some_controller.php
+++ utils-tests/Rector/Rector/ClassMethod/SymfonyRouteAttributesToLaravelRouteFileRector/Fixture/some_controller.php
@@ -2,11 +2,8 @@
 
 namespace Rector\Website\Utils\Tests\Rector\Rector\ClassMethod\SymfonyRouteAttributesToLaravelRouteFileRector\Fixture;
 
-use Symfony\Component\Routing\Annotation\Route;
-
 class SomeController
 {
-    #[Route(path: '/some', name: 'some')]
     public function __invoke()
     {
         $this->someMethod();
