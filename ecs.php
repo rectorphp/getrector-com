dd28799988a3885817c9556de7ca49d732046bd9
Tomas Votruba <tomas.vot@gmail.com>
2024-01-10 22:39:00 +0100

    Bump to new ECSConfig (#1921)
    * bump to new ECSConfig
    
    * json is part of PHP 8.0+, lock down to php-parse 4.18 to fix early upgade

--- ecs.php
+++ ecs.php
@@ -3,18 +3,9 @@
 declare(strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
-        __DIR__ . '/config',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-        __DIR__ . '/ecs.php',
-        __DIR__ . '/rector.php',
-    ]);
-
-    $ecsConfig->sets([SetList::PSR_12, SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE]);
-
-    $ecsConfig->skip([__DIR__ . '/config/bundles.php', '*/Fixture/*', '*/Expected/*']);
-};
+return ECSConfig::configure()
+    ->withPaths([__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/tests'])
+    ->withRootFiles()
+    ->withPreparedSets(psr12: true, common: true, symplify: true)
+    ->withSkip([__DIR__ . '/config/bundles.php', '*/Fixture/*', '*/Expected/*']);
