8bb6d47948156af4df09f07c97b5a00f7f3c337e
Tomas Votruba <tomas.vot@gmail.com>
2024-01-11 02:08:06 +0100

    highlight using highlight js directly

--- resources/views/_snippets/styles.blade.php
+++ resources/views/_snippets/styles.blade.php
@@ -4,4 +4,35 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.28.0/codemirror.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.28.0/theme/solarized.min.css">
 
+{{-- code highligh posts --}}
+{{-- pick from https://highlightjs.org/demo --}}
+{{-- see ChatGPT https://chat.openai.com/share/af70716e-067c-481c-ad61-fc40de2f4dc3 --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/atlas.min.css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/php.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/bash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/yaml.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/diff.min.js"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', (event) => {
+        document.querySelectorAll('pre code.language-php').forEach((block) => {
+            hljs.highlightBlock(block);
+        });
+
+        document.querySelectorAll('pre code.language-yaml').forEach((block) => {
+            hljs.highlightBlock(block);
+        });
+
+        document.querySelectorAll('pre code.language-bash').forEach((block) => {
+            hljs.highlightBlock(block);
+        });
+
+        document.querySelectorAll('pre code.language-diff').forEach((block) => {
+            hljs.highlightBlock(block);
+        });
+    });
+</script>
+
 @vite(['resources/css/app.scss', 'resources/js/app.js'])
