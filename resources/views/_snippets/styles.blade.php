47581bd328d4fd160c966b951f75b19c404bdb7a
Tomas Votruba <tomas.vot@gmail.com>
2024-01-11 02:14:57 +0100

    round cordners

--- resources/views/_snippets/styles.blade.php
+++ resources/views/_snippets/styles.blade.php
@@ -8,6 +8,11 @@
 {{-- pick from https://highlightjs.org/demo --}}
 {{-- see ChatGPT https://chat.openai.com/share/af70716e-067c-481c-ad61-fc40de2f4dc3 --}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/atlas.min.css">
+<style>
+    pre code.hljs {
+        border-radius: .6em;
+    }
+</style>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/php.min.js"></script>
