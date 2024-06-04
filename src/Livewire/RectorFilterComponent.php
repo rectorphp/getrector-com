520de87b34a2c4d0baf15f69aec5c261710cd5f5
Tomas Votruba <tomas.vot@gmail.com>
2024-06-04 18:52:03 +0900

    exclude dowgnrade rulres

--- src/Livewire/RectorFilterComponent.php
+++ src/Livewire/RectorFilterComponent.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Rector\Website\Livewire;
 
 use Illuminate\View\View;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Rector\Website\FileSystem\RectorFinder;
 use Rector\Website\RuleFilter\RuleFilter;
 
 final class RectorFilterComponent extends Component
 {
+    #[Url]
     public ?string $query = null;
 
     public function render(): View
