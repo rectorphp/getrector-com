9161d5f162fde825da5e70a73f583e7cae18e5c4
Tomas Votruba <tomas.vot@gmail.com>
2024-06-17 22:49:04 +0900

    make ast componetnable

--- resources/views/ast/ast_detail.blade.php
+++ resources/views/ast/ast_detail.blade.php
@@ -5,43 +5,5 @@
 @endphp
 
 @section('main')
-    <div class="float-end mt-4">
-        <a href="{{ action(\Rector\Website\Controller\Ast\AstController::class) }}"
-           class="btn btn-outline-success" style="margin-top: -.7em">
-            ← Create new code
-        </a>
-    </div>
-
-    <div class="mt-4 mb-5" style="min-height: 30em">
-        <p>Click on code part to see its AST</p>
-
-        <div id="clickable-nodes-code" class="mb-4">
-            <pre><code class="hljs">&lt;?php
-
-{!! $matrix_vision !!}</code></pre>
-
-        </div>
-
-        <br>
-
-        <p>Selected code is represented by following abstract syntax tree:</p>
-
-        <div class="row">
-            <div class="col-12">
-                <pre><code class="language-php">{{ $simple_node_dump }}</code></pre>
-            </div>
-        </div>
-
-        <br>
-
-        @if ($target_node_class)
-            <p>
-                What class to put into <code>Rector::getNodeTypes()</code> method to hook into?
-            </p>
-
-            <input type="text" class="form-control" onClick="this.select();" value="{{ $target_node_class }}::class" style="width: 25em">
-        @endif
-
-        <br>
-    </div>
+    @livewire('ast-component', ['astRun' => $astRun])
 @endsection
--- src/Controller/Ast/AstDetailController.php
+++ src/Controller/Ast/AstDetailController.php
@@ -7,28 +7,13 @@ namespace Rector\Website\Controller\Ast;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
 use Illuminate\View\View;
-use PhpParser\Node;
-use PhpParser\Node\Attribute;
-use PhpParser\Node\AttributeGroup;
-use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Name;
-use PhpParser\Node\Param;
-use PhpParser\Node\Stmt;
-use PhpParser\Node\Stmt\UseUse;
-use Rector\CustomRules\SimpleNodeDumper;
 use Rector\Website\Entity\AstRun;
-use Rector\Website\PhpParser\ClickablePrinter;
-use Rector\Website\PhpParser\NodeResolver\FocusedNodeResolver;
-use Rector\Website\PhpParser\SimplePhpParser;
 use Rector\Website\Repository\AstRunRepository;
 
 final class AstDetailController extends Controller
 {
     public function __construct(
         private readonly AstRunRepository $astRunRepository,
-        private readonly SimplePhpParser $simplePhpParser,
-        private readonly FocusedNodeResolver $focusedNodeResolver,
     ) {
     }
 
@@ -43,70 +28,9 @@ final class AstDetailController extends Controller
             );
         }
 
-        $nodes = $this->simplePhpParser->parseString($astRun->getContent());
-
-        $focusedNode = is_int($activeNodeId) && $activeNodeId > 0
-            ? $this->focusedNodeResolver->focus($nodes, $activeNodeId)
-            : null;
-
-        if ($focusedNode instanceof Node) {
-            $simpleNodeDump = SimpleNodeDumper::dump($focusedNode);
-            $targetNodeClass = $this->resolveTargetNodeClass($focusedNode);
-        } else {
-            $simpleNodeDump = SimpleNodeDumper::dump($nodes);
-            $targetNodeClass = null;
-        }
-
         return \view('ast/ast_detail', [
             'page_title' => 'Play with AST',
-            'matrix_vision' => $this->makeNodeClickable($nodes, $uuid, $activeNodeId),
-            'simple_node_dump' => $simpleNodeDump,
-            'active_node_id' => $activeNodeId,
-            'target_node_class' => $targetNodeClass,
+            'astRun' => $astRun,
         ]);
-    }
-
-    /**
-     * @param Node[] $nodes
-     */
-    private function makeNodeClickable(array $nodes, string $uuid, ?int $activeNodeId): string
-    {
-        $clickablePrinter = new ClickablePrinter($uuid, $activeNodeId);
-        return $clickablePrinter->prettyPrint($nodes);
-    }
-
-    private function resolveTargetNodeClass(Node $node): string
-    {
-        if ($node instanceof UseUse || $node instanceof AttributeGroup) {
-            $parentNode = $node->getAttribute('parent');
-            return $parentNode::class;
-        }
-
-        if ($node instanceof Attribute) {
-            $attributeGroup = $node->getAttribute('parent');
-            $stmt = $attributeGroup->getAttribute('parent');
-            return $stmt::class;
-        }
-
-        if ($node instanceof Stmt) {
-            return $node::class;
-        }
-
-        if ($node instanceof Variable) {
-            $parentNode = $node->getAttribute('parent');
-
-            // special case
-            if ($parentNode instanceof Param) {
-                return $parentNode::class;
-            }
-        }
-
-        // target one level up
-        if ($node instanceof Identifier || $node instanceof Name || $node instanceof Variable) {
-            $parentNode = $node->getAttribute('parent');
-            return $this->resolveTargetNodeClass($parentNode);
-        }
-
-        return $node::class;
     }
 }
