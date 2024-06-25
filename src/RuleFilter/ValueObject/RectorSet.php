9139400b475266bdcd68845b56b80fcda7e96431
Tomas Votruba <tomas.vot@gmail.com>
2024-06-25 11:45:31 +0900

    make use of set providers

--- src/RuleFilter/ValueObject/RectorSet.php
+++ src/RuleFilter/ValueObject/RectorSet.php
@@ -4,59 +4,20 @@ declare(strict_types=1);
 
 namespace App\RuleFilter\ValueObject;
 
-use Nette\Utils\Strings;
+use Rector\Contract\Rector\RectorInterface;
 
-final class RectorSet
+final readonly  class RectorSet
 {
-    private string $humanName;
-
     /**
-     * @param string[] $rectorClasses
+     * @param string $groupName
+     * @param string $name
+     * @param array<class-string<RectorInterface>> $rectorClasses
      */
     public function __construct(
-        private readonly string $constantName,
-        private readonly array $rectorClasses,
-        private readonly string $groupName
+        private string $groupName,
+        private string $name,
+        private array $rectorClasses,
     ) {
-        if (str_starts_with($constantName, 'PHP_')) {
-            $match = Strings::match($constantName, '#(?<major>\d)(?<minor>\d)#');
-            if ($match === null) {
-                $this->humanName = 'Polyfills';
-                return;
-            }
-
-            $this->humanName = 'PHP ' . $match['major'] . '.' . $match['minor'];
-            return;
-        }
-
-        if (str_starts_with($this->constantName, 'TWIG_') || str_starts_with(
-            $this->constantName,
-            'PHPUNIT_'
-        ) || str_starts_with($this->constantName, 'DOCTRINE_')) {
-            $match = Strings::match($this->constantName, '#(?<name>.*?)_(?<major>\d)(?<minor>\d+)$#');
-            if ($match) {
-                $name = $match['name'];
-                $name = str_replace(['PHPUNIT', 'DOCTRINE_'], ['PHPUnit', 'Doctrine '], $name);
-
-                // only exception for PHPUnit
-                $humanVersion = $name === 'PHPUnit' && $match['major'] === '1' ? '10.0' : $match['major'] . '.' . $match['minor'];
-
-                $this->humanName = $name . ' ' . $humanVersion;
-                return;
-            }
-        }
-
-        if (str_starts_with($this->constantName, 'SYMFONY_')) {
-            $match = Strings::match($this->constantName, '#(?<major>\d)(?<minor>\d)$#');
-            if ($match) {
-                $this->humanName = 'Symfony ' . $match['major'] . '.' . $match['minor'];
-                return;
-            }
-        }
-
-        // human name
-        // uppercase to first letter of each word upper only
-        $this->humanName = ucwords(strtolower(str_replace('_', ' ', $this->constantName)));
     }
 
     public function hasRule(string $rectorClass): bool
@@ -66,13 +27,7 @@ final class RectorSet
 
     public function getSlug(): string
     {
-        return strtolower($this->constantName);
-    }
-
-    public function getName(): string
-    {
-        // @todo figure out human-readbale set name
-        return str_replace('_', ' ', $this->constantName);
+        return strtolower($this->groupName) .  '_' . strtolower($this->name);
     }
 
     public function getGroupName(): string
@@ -80,8 +35,8 @@ final class RectorSet
         return $this->groupName;
     }
 
-    public function getHumanName(): string
+    public function getName(): string
     {
-        return $this->humanName;
+        return $this->name;
     }
 }
--- src/Sets/RectorSetsTreeProvider.php
+++ src/Sets/RectorSetsTreeProvider.php
@@ -6,20 +6,11 @@ namespace App\Sets;
 
 use App\PhpParser\SimplePhpParser;
 use App\RuleFilter\ValueObject\RectorSet;
-use App\Utils\ClassNameResolver;
-use Nette\Utils\Strings;
-use PhpParser\Node;
-use PhpParser\Node\Name;
-use PhpParser\NodeFinder;
-use Rector\Config\Level\CodeQualityLevel;
-use Rector\Config\Level\DeadCodeLevel;
-use Rector\Config\Level\TypeDeclarationLevel;
-use Rector\Set\Contract\SetListInterface;
-use Rector\Set\ValueObject\SetList;
-use ReflectionClass;
-use Symfony\Component\Finder\Finder;
-use Symfony\Component\Finder\SplFileInfo;
-use Webmozart\Assert\Assert;
+use Rector\Doctrine\Set\SetProvider\DoctrineSetProvider;
+use Rector\PHPUnit\Set\SetProvider\PHPUnitSetProvider;
+use Rector\Set\Contract\SetInterface;
+use Rector\Set\SetProvider\CoreSetProvider;
+use Rector\Symfony\Set\SetProvider\SymfonySetProvider;
 
 final class RectorSetsTreeProvider
 {
@@ -28,11 +19,6 @@ final class RectorSetsTreeProvider
      * @var RectorSet[]
      */
     private array $rectorSets = [];
-
-    public function __construct(
-        private SimplePhpParser $simplePhpParser,
-    ) {
-    }
 
     /**
      * @return array<string, RectorSet[]>
@@ -56,123 +42,64 @@ final class RectorSetsTreeProvider
             return $this->rectorSets;
         }
 
+        $simplePhpParser = new SimplePhpParser();
+
+        $setProviders = [
+            new CoreSetProvider(),
+            new SymfonySetProvider(),
+            new PHPUnitSetProvider(),
+            new DoctrineSetProvider(),
+        ];
+
         $rectorSets = [];
+        foreach ($setProviders as $setProvider) {
+            foreach ($setProvider->provide() as $set) {
 
-        // @todo possibly cache or prebuild this?
-        foreach ($this->findSetListFileInfos() as $setListFileInfo) {
-            // no level sets, as deprecated
-            if (str_contains($setListFileInfo->getRealPath(), 'Level')) {
-                continue;
-            }
-
-            $setListClassName = $this->resolveClassName($setListFileInfo);
-            $setListReflectionClass = new ReflectionClass($setListClassName);
-
-            foreach ($setListReflectionClass->getConstants() as $constantName => $constantValue) {
-                // skipped as internal
-                if ($constantName === 'RECTOR_PRESET') {
-                    continue;
-                }
-
-                $groupName = $this->resolveGroupName($setListReflectionClass, $constantName);
-
-                // step by step level reference
-                if ($constantName === 'CODE_QUALITY') {
-                    $rectorClasses = CodeQualityLevel::RULES;
-                } elseif ($constantName === 'DEAD_CODE') {
-                    $rectorClasses = DeadCodeLevel::RULES;
-                } elseif ($constantName === 'TYPE_DECLARATION') {
-                    $rectorClasses = TypeDeclarationLevel::RULES;
-                } else {
-                    $rectorClasses = $this->resolveRectorClassesFromSetFilePath($constantValue);
-                }
-
-                $rectorSets[] = new RectorSet($constantName, $rectorClasses, $groupName);
+                new RectorSet();
+                // SimplePhpParser $simplePhpParser
             }
         }
+
+        //                // step by step level reference
+        //                if ($constantName === 'CODE_QUALITY') {
+        //                    $rectorClasses = CodeQualityLevel::RULES;
+        //                } elseif ($constantName === 'DEAD_CODE') {
+        //                    $rectorClasses = DeadCodeLevel::RULES;
+        //                } elseif ($constantName === 'TYPE_DECLARATION') {
+        //                    $rectorClasses = TypeDeclarationLevel::RULES;
+        //                } else {
+        //                    $rectorClasses = $this->resolveRectorClassesFromSetFilePath($constantValue);
+        //                }
+        //
+        //                $rectorSets[] = new RectorSet($constantName, $rectorClasses, $groupName);
+        //            }
+        //        }
 
         $this->rectorSets = $rectorSets;
 
         return $rectorSets;
     }
 
-    /**
-     * @return SplFileInfo[]
-     */
-    private function findSetListFileInfos(): array
-    {
-        // @todo possibly add to SetProvider to use directly without any this magic
-        $setsListFileIterator = Finder::create()
-            ->files()
-            ->in(__DIR__ . '/../../vendor/rector/rector/src/Set')
-            ->in(__DIR__ . '/../../vendor/rector/rector/vendor/rector/rector-doctrine/src/Set')
-            ->in(__DIR__ . '/../../vendor/rector/rector/vendor/rector/rector-symfony/src/Set')
-            ->in(__DIR__ . '/../../vendor/rector/rector/vendor/rector/rector-phpunit/src/Set')
-            // @todo add unofficial extensions as well
-            ->name('*SetList.php')
-            ->files()
-            ->sortByName()
-            ->getIterator();
-
-        return iterator_to_array($setsListFileIterator);
-    }
-
-    /**
-     * @return class-string<SetListInterface>
-     */
-    private function resolveClassName(SplFileInfo $fileInfo): string
-    {
-        $className = ClassNameResolver::resolveFromFileContents($fileInfo->getContents(), $fileInfo->getRealPath());
-        Assert::classExists($className);
-
-        return $className;
-    }
-
-    /**
-     * @return string[]
-     */
-    private function resolveRectorClassesFromSetFilePath(string $constantValue): array
-    {
-        Assert::fileExists($constantValue);
-
-        $nodes = $this->simplePhpParser->parseFile($constantValue);
-        $nodeFinder = new NodeFinder();
-
-        $rectorClassesNames = $nodeFinder->find($nodes, function (Node $node): bool {
-            if (! $node instanceof Name) {
-                return false;
-            }
-
-            return str_ends_with($node->toString(), 'Rector');
-        });
-
-        $rectorClasses = [];
-
-        /** @var Name[] $rectorClassesNames */
-        foreach ($rectorClassesNames as $rectorClassName) {
-            $rectorClasses[] = $rectorClassName->toString();
-        }
-
-        return $rectorClasses;
-    }
-
-    /**
-     * @param ReflectionClass<SetListInterface> $setListReflectionClass
-     */
-    private function resolveGroupName(ReflectionClass $setListReflectionClass, string $constantName): string
-    {
-        if (str_starts_with($constantName, 'PHP_')) {
-            return 'PHP';
-        }
-
-        if ($setListReflectionClass->getName() === SetList::class) {
-            return 'Core';
-        }
-
-        // rector split package
-        $match = Strings::match($setListReflectionClass->getName(), '#\\\\(?<name>[\w]+)SetList#');
-        Assert::isArray($match);
-
-        return $match['name'];
-    }
+    //    /**
+    //     * @return string[]
+    //     */
+    //    private function resolveRectorClassesFromSetFilePath(string $constantValue): array
+    //    {
+    //        $rectorClassesNames = $nodeFinder->find($nodes, function (Node $node): bool {
+    //            if (! $node instanceof Name) {
+    //                return false;
+    //            }
+    //
+    //            return str_ends_with($node->toString(), 'Rector');
+    //        });
+    //
+    //        $rectorClasses = [];
+    //
+    //        /** @var Name[] $rectorClassesNames */
+    //        foreach ($rectorClassesNames as $rectorClassName) {
+    //            $rectorClasses[] = $rectorClassName->toString();
+    //        }
+    //
+    //        return $rectorClasses;
+    //    }
 }
