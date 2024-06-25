9ebe64eb2f6d090809d1e9aa3a07ca8fa3f4e8a9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-06-25 13:17:17 +0700

    clean up skip config on rector.php (#2370)

--- rector.php
+++ rector.php
@@ -22,7 +22,4 @@ return RectorConfig::configure()
         '*/Expected/*',
         // generated
         __DIR__ . '/src/Ast/PhpParser/ClickablePrinter.php',
-
-        // cause infinite loop on local with macOS
-        __DIR__ . '/_ide_helper.php',
     ]);
