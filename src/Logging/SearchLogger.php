b460ddae48b1a4c42f54b760da209bac99ca5417
Tomas Votruba <tomas.vot@gmail.com>
2024-07-04 18:52:40 +0200

    skip SQL injection attacks from logging, as no value

--- src/Logging/SearchLogger.php
+++ src/Logging/SearchLogger.php
@@ -9,11 +9,30 @@ use Nette\Utils\Json;
 final class SearchLogger
 {
     /**
+     * Skip typical SQL injection attacks, as no value
+     * @var string[]
+     */
+    private const EXCLUDED_QUERIES = ['ORDER BY', 'SELECT', ' AND ', ' OR '];
+
+    /**
      * Simple search logger, to see what is needed by the community
      * and help to cover it with rules
      */
     public function log(?string $query, ?string $nodeType, ?string $set): void
     {
+        if ($query === null && $nodeType) {
+            return;
+        }
+
+        // skip typical SQL injections attacks
+        if ($query) {
+            foreach (self::EXCLUDED_QUERIES as $excludedQuery) {
+                if (str_contains($query, $excludedQuery)) {
+                    return;
+                }
+            }
+        }
+
         $searchJson = Json::encode([
             'timestamp' => now(),
             'query' => $query,
