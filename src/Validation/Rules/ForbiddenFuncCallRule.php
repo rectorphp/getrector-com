549815d7fbe8032d6ba16cebb4fbec8f876c5394
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-07-18 04:14:26 +0700

    try call

--- src/Validation/Rules/ForbiddenFuncCallRule.php
+++ src/Validation/Rules/ForbiddenFuncCallRule.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\Validation\Rules;
 
 use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Reflection\FunctionReflection;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 use PhpParser\Error;
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name\FullyQualified;
 use PhpParser\NodeFinder;
 use PhpParser\ParserFactory;
 use Rector\DependencyInjection\LazyContainerFactory;
@@ -22,8 +25,8 @@ final class ForbiddenFuncCallRule implements ValidationRule
         $parser = $parserFactory->create(ParserFactory::PREFER_PHP7);
 
         $lazyContainerFactory = new LazyContainerFactory();
-        $rectorConfig = $lazyContainerFactory->create();
-        $phpstanReflectionProvider = $rectorConfig->make(ReflectionProvider::class);
+        $rectorContainer = $lazyContainerFactory->create();
+        $phpstanReflectionProvider = $rectorContainer->make(ReflectionProvider::class);
 
         try {
             $stmts = $parser->parse($value);
@@ -32,7 +35,32 @@ final class ForbiddenFuncCallRule implements ValidationRule
             $funcCall = $nodeFinder->findFirst(
                 (array) $stmts,
                 function (Node $subNode) use ($phpstanReflectionProvider): bool {
-                    return $subNode instanceof FuncCall;
+                    if (! $subNode instanceof FuncCall) {
+                        return false;
+                    }
+
+                    // dynamic name? can be evil...
+                    if ($subNode->name instanceof Expr) {
+                        return true;
+                    }
+
+                    $namespaceName = $subNode->name->getAttribute('namespaced_name');
+                    $functionReflection = null;
+
+                    if ($namespaceName instanceof FullyQualified) {
+                        if ($phpstanReflectionProvider->hasFunction($namespaceName, null)) {
+                            $functionReflection = $phpstanReflectionProvider->getFunction($namespaceName, null);
+                        }
+                    } else {
+                        $functionReflection = $phpstanReflectionProvider->getFunction($subNode->name, null);
+                    }
+
+                    // another possible evil..
+                    if (! $functionReflection instanceof FunctionReflection) {
+                        return true;
+                    }
+
+                    return $functionReflection->hasSideEffects()->yes();
                 }
             );
 
