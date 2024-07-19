d6409a2549472df23a5f5789d3e7656c6587363b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-07-19 20:03:27 +0700

    cast

--- src/Validation/Rules/ForbiddenCallLikeRule.php
+++ src/Validation/Rules/ForbiddenCallLikeRule.php
@@ -77,7 +77,7 @@ final class ForbiddenCallLikeRule implements ValidationRule
                     }
 
                     // non class should be safe
-                    $className = $type->getClassName();
+                    $className = (string) $type->getClassName();
                     if (! class_exists($className)) {
                         return false;
                     }
