ebb6732a51414fa9aaf217af973fc09be0aac16f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-07-19 19:22:35 +0700

    [validation] Add ForbiddenCallLikeRule

---
+++ src/Validation/Rules/ForbiddenCallLikeRule.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Validation\Rules;
+
+use PHPStan\Type\ObjectType;
+use ReflectionClass;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use PhpParser\Error;
+use PhpParser\Node;
+use PhpParser\Node\Expr\CallLike;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\NullsafeMethodCall;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\NodeFinder;
+use PhpParser\ParserFactory;
+use Rector\DependencyInjection\LazyContainerFactory;
+use Rector\NodeTypeResolver\PHPStan\Scope\ScopeFactory;
+
+final class ForbiddenCallLikeRule implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $parserFactory = new ParserFactory();
+        $parser = $parserFactory->create(ParserFactory::PREFER_PHP7);
+
+        $lazyContainerFactory = new LazyContainerFactory();
+        $rectorConfig = $lazyContainerFactory->create();
+
+        /** @var ScopeFactory $scopeFactory */
+        $scopeFactory = $rectorConfig->make(ScopeFactory::class);
+        $mutatingScope = $scopeFactory->createFromFile(__FILE__);
+
+        try {
+            $stmts = $parser->parse($value);
+            $nodeFinder = new NodeFinder();
+
+            $callLike = $nodeFinder->findFirst(
+                (array) $stmts,
+                function (Node $subNode) use ($mutatingScope): bool {
+                    // already covered by ForbiddenFuncCallRule
+                    if ($subNode instanceof FuncCall) {
+                        return false;
+                    }
+
+                    if (! $subNode instanceof CallLike) {
+                        return false;
+                    }
+
+                    /** @var MethodCall|StaticCall|New_|NullsafeMethodCall $subNode */
+                    $type = $subNode instanceof StaticCall
+                        ? $mutatingScope->getType($mutatingScope->class)
+                        : $mutatingScope->getType($subNode->var);
+
+                    // magic!
+                    if (! $type instanceof ObjectType) {
+                        return true;
+                    }
+
+                    $reflectionClass = new ReflectionClass($type->getClassName());
+                    // @todo:
+                    // 1. verify allowed CallLike, eg: new \PHPStan\Type\MixedType(),
+                    // 2. verify not allowed CallLike, eg: (new Nette\Utils\FileSystem)->write('test.php', 'test');
+
+                    return !$reflectionClass->isInternal();
+                }
+            );
+
+            if ($callLike instanceof CallLike) {
+                $fail('PHP config should not include side effect call like');
+            }
+        } catch (Error $error) {
+            $fail(sprintf('PHP code is invalid: %s', $error->getMessage()));
+        }
+    }
+}
