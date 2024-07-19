c0b6623e3d7dcfdef90462387f897ac7f0b1304a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-07-19 21:20:04 +0700

    paths

--- tests/Controller/DemoControllerTest.php
+++ tests/Controller/DemoControllerTest.php
@@ -131,8 +131,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__ . '/src']);
-
     $rectorConfig->sets([\Rector\Set\ValueObject\SetList::CODE_QUALITY]);
 };
 
