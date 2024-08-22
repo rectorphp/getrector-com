6ca2d6b6d20ba06a2a135e622af5b056fa2f6a31
Tomas Votruba <tomas.vot@gmail.com>
2024-08-22 15:34:41 +0200

    add simple stats

--- resources/views/stats/find_rule_stats.blade.php
+++ resources/views/stats/find_rule_stats.blade.php
@@ -1,2 +1,36 @@
-Check :)
+<h2>Node finder stats</h2>
+
+<h3>Queries</h3>
+
+<ul>
+@foreach ($queriesToCount as $query => $count)
+    <li>
+        {{ $query }}: {{ $count }}
+    </li>
+@endforeach
+</ul>
+
+<hr>
+
+<h3>Node Types</h3>
+
+<ul>
+@foreach ($nodeTypesToCount as $nodeType => $count)
+    <li>
+        {{ $nodeType }}: {{ $count }}
+    </li>
+@endforeach
+</ul>
+
+<hr>
+
+<h3>Sets</h3>
+
+<ul>
+    @foreach ($setsToCount as $set => $count)
+        <li>
+            {{ $set }}: {{ $count }}
+        </li>
+    @endforeach
+</ul>
 
--- src/Controller/Stats/FindRuleStatsController.php
+++ src/Controller/Stats/FindRuleStatsController.php
@@ -12,19 +12,67 @@ final class FindRuleStatsController extends Controller
 {
     public function __invoke()
     {
-        $searchFileContents = FileSystem::read(storage_path('logs/search.json'));
-        $searchFileLines = explode(PHP_EOL, $searchFileContents);
-        foreach ($searchFileLines as $searchFileLine) {
+        $searchLogFilePath = storage_path('logs/search.json');
+        $searchRecords = $this->loadFileToJsonItems($searchLogFilePath);
+
+        $nodeTypes = $this->getArrayFlattenKey($searchRecords, 'nodeType');
+        $sets = $this->getArrayFlattenKey($searchRecords, 'set');
+        $queries = $this->getArrayFlattenKey($searchRecords, 'query');
+
+        $queriesToCount = $this->groupToCount($queries);
+        $setsToCount = $this->groupToCount($sets);
+        $nodeTypesToCount = $this->groupToCount($nodeTypes);
+
+        return view('stats.find_rule_stats', [
+            'queriesToCount' => $queriesToCount,
+            'setsToCount' => $setsToCount,
+            'nodeTypesToCount' => $nodeTypesToCount,
+        ]);
+    }
+
+    /**
+     * @param mixed[] $items
+     */
+    private function getArrayFlattenKey(array $items, string $keyName): array
+    {
+        return array_map(function (array $item) use ($keyName) {
+            return $item[$keyName];
+        }, $items);
+    }
+
+    /**
+     * @param mixed[] $items
+     * @return mixed[]
+     */
+    private function groupToCount(array $items): array
+    {
+        $nonEmptyItems = array_filter($items);
+
+        $itemsToValues = array_count_values($nonEmptyItems);
+        arsort($itemsToValues);
+
+        return $itemsToValues;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    private function loadFileToJsonItems(string $filePath): array
+    {
+        $fileContents = FileSystem::read($filePath);
+
+        $fileLines = explode(PHP_EOL, $fileContents);
+
+        $items = [];
+        foreach ($fileLines as $searchFileLine) {
+            // skip empty
             if (trim($searchFileLine) === '') {
                 continue;
             }
 
-            $searchLine = Json::decode($searchFileLine, true);
-            dump($searchLine);
+            $items[] = Json::decode($searchFileLine, true);
         }
 
-        die;
-
-        return view('stats.find_rule_stats');
+        return $items;
     }
 }
