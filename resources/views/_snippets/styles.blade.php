813c8e7c2b5bc5df8df8177400ff6c371690ce05
Tomas Votruba <tomas.vot@gmail.com>
2024-09-09 08:11:20 +0200

    add diff toggle in footer

--- resources/views/_snippets/styles.blade.php
+++ resources/views/_snippets/styles.blade.php
@@ -24,8 +24,6 @@
 </style>
 
 
-<button onclick="toggleTheme()">Toggle theme</button>
-
 <script>
     // Function to load theme from local storage
     function loadTheme() {
@@ -33,20 +31,32 @@
         if (theme) {
             document.getElementById('theme-link').setAttribute('href', theme);
         }
+
+        const theme_toggle_text = localStorage.getItem('theme_toggle_text');
+        if (theme_toggle_text) {
+            document.getElementById('theme_toggle').textContent = theme_toggle_text;
+        }
     }
 
     // Function to toggle theme and save preference to local storage
     function toggleTheme() {
         var theme = document.getElementById('theme-link');
+        var theme_toggle = document.getElementById('theme_toggle');
         var lightTheme = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/atelier-dune-light.min.css';
         var darkTheme = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/atlas.min.css';
 
         if (theme.getAttribute('href') === darkTheme) {
             theme.setAttribute('href', lightTheme);
             localStorage.setItem('theme', lightTheme);
+            localStorage.setItem('theme_toggle_text', 'Dark Code Theme');
+
+            theme_toggle.textContent = 'Dark Code Theme';
         } else {
             theme.setAttribute('href', darkTheme);
             localStorage.setItem('theme', darkTheme);
+            localStorage.setItem('theme_toggle_text', 'Ligth Code Theme');
+
+            theme_toggle.textContent = 'Ligth Code Theme';
         }
     }
 
