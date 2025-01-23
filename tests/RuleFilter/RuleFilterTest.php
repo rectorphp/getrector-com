3882f42608f654246e822095ecabdb3afebd4fd4
Geni Jaho <jahogeni@gmail.com>
2025-01-23 10:38:11 +0100

    Fix Laravel rule filtering (#2800)
    * Fix filtering for Laravel rules
    
    * Use dev-main for Rector Laravel

---
+++ tests/RuleFilter/RuleFilterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\RuleFilter;
+
+use App\Enum\FindRule\GroupName;
+use App\RuleFilter\RuleFilter;
+use App\RuleFilter\ValueObject\RuleMetadata;
+use App\Tests\AbstractTestCase;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use RectorLaravel\Rector\ClassMethod\MigrateToSimplifiedAttributeRector;
+
+final class RuleFilterTest extends AbstractTestCase
+{
+    public function testFilterBySetGroup(): void
+    {
+        $ruleMetadataCore = new RuleMetadata(
+            RenameMethodRector::class,
+            'Some description',
+            [],
+            [],
+            'some-rector.php'
+        );
+        $ruleMetadataCommunity = new RuleMetadata(
+            MigrateToSimplifiedAttributeRector::class,
+            'Some description',
+            [],
+            [],
+            'some-rector.php'
+        );
+
+        $ruleFilter = $this->make(RuleFilter::class);
+        $filtered = $ruleFilter->filter(
+            [$ruleMetadataCore, $ruleMetadataCommunity],
+            null,
+            null,
+            GroupName::LARAVEL,
+        );
+
+        $this->assertCount(1, $filtered);
+        $this->assertSame($ruleMetadataCommunity->getRectorClass(), $filtered[0]->getRectorClass());
+    }
+}
