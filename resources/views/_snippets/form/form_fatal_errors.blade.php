79ee475b087790794b329be9bd290225f0aff119
Tomas Votruba <tomas.vot@gmail.com>
2025-04-15 16:13:05 +0200

    move buttons to left to allign all metadata in single column (#2956)

--- resources/views/_snippets/form/form_fatal_errors.blade.php
+++ resources/views/_snippets/form/form_fatal_errors.blade.php
@@ -1,19 +1,21 @@
 @if ($rectorRun->hasRun() && $rectorRun->isSuccessful() !== true)
-    <div class="alert alert-danger mb-3">
-        <p>
-            <strong>Rector run Failed:</strong>
-        </p>
+    <div class="row">
+        <div class="alert alert-danger mb-3">
+            <p>
+                <strong>Rector run Failed:</strong>
+            </p>
 
-        {!! $rectorRun->getFatalErrorMessage() !!}
+            {!! $rectorRun->getFatalErrorMessage() !!}
 
-        @if ($rectorRun->getErrors() !== [])
-            <ul class="mt-3 ms-0 pl-4">
-                @foreach ($rectorRun->getErrors() as $error)
-                    <li>
-                        {{ $error }}
-                    </li>
-                @endforeach
-            </ul>
-        @endif
+            @if ($rectorRun->getErrors() !== [])
+                <ul class="mt-3 ms-0 pl-4">
+                    @foreach ($rectorRun->getErrors() as $error)
+                        <li>
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
     </div>
 @endif
