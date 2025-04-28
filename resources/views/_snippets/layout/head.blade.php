52f8e802a2761d8d7cec4fc3edc78ed46927123b
Tomas Votruba <tomas.vot@gmail.com>
2025-04-29 01:13:14 +0200

    tv metas (#2985)
    * add meta title + description to particular pages
    
    * mics

--- resources/views/_snippets/layout/head.blade.php
+++ resources/views/_snippets/layout/head.blade.php
@@ -1,14 +1,27 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="{{ \App\Enum\Design::MAIN_TITLE }}" />
-<meta name="keywords" content="php, rector, instant upgrades, instant refactoring, upgrade symfony, upgrade php, upgrade cakephp, upgrade legacy, php migration, laravel shift alternative, php shift" />
 
-@php
-    $full_title = (isset($page_title) ? $page_title . ' | ' : '') . \App\Enum\Design::MAIN_TITLE;
-@endphp
+@isset ($metaDescription)
+    <meta name="description" content="{{ $metaDescription }}" />
+    <meta property="og:description" content="{{ $metaDescription }}" >
+@else
+    <meta name="description" content="{{ \App\Enum\Design::MAIN_TITLE }}" />
+@endisset
+    <meta name="keywords" content="php, rector, instant upgrades, instant refactoring, upgrade symfony, upgrade php, upgrade cakephp, upgrade legacy, php migration, laravel shift alternative, php shift" />
 
-<title>{{ $full_title }}</title>
+@isset ($metaTitle)
+    <title>{{ $metaTitle }}</title>
+    <meta property="og:title" content="{{ $metaTitle }}">
+    <meta property="twitter:title" content="{{ $metaTitle }}">
+@else
+    @php
+        $full_title = (isset($page_title) ? $page_title . ' | ' : '') . \App\Enum\Design::MAIN_TITLE;
+    @endphp
+    <title>{{ $full_title }}</title>
+    <meta property="og:title" content="{{ $full_title }}">
+    <meta property="twitter:title" content="{{ $full_title }}">
+@endisset
 
 <link rel="alternate" type="application/rss+xml" title="Rector Blog RSS" href="/rss.xml">
 
@@ -21,9 +34,6 @@
 @else
     <!-- default social tags -->
     <meta property="twitter:card" content="summary">
-
-    <meta property="og:title" content="{{ $full_title }}">
-    <meta property="twitter:title" content="{{ $full_title }}">
 
     <meta property="og:image" content="{{ \App\Enum\Design::SOCIAL_RECTOR_LOGO }}">
     <meta name="twitter:image" content="{{ \App\Enum\Design::SOCIAL_RECTOR_LOGO }}">
