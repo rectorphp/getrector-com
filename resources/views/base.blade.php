8aa7696fd66ea8745029a85c28da0967b36913d5
Tomas Votruba <tomas.vot@gmail.com>
2025-04-30 01:41:59 +0200

    use cdn to load twitter bootstrap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
     <!-- build timestamp: {{ date("Y-m-d, H:i:s") }} -->
     <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
         @include('_snippets/layout/head')
         @include('_snippets/styles')
 
