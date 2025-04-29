0d353c384458fc49b9582ec318c9f2e121f6928d
Tomas Votruba <tomas.vot@gmail.com>
2025-04-30 00:35:05 +0200

    add post links

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -1,3 +1,7 @@
+@php
+    /** @var \App\Entity\Post $recentPosts */
+@endphp
+
 @extends('base')
 
 @section('main')
@@ -119,6 +123,27 @@
             -->
 
             @include('_snippets/homepage_separator')
+
+            <h3>Featured</h3>
+            @foreach ($recentPosts as $recentPost)
+                @php /** @var \App\Entity\Post $recentPost */ @endphp
+
+                <a href="{{ action(\App\Controller\Blog\PostController::class, ['postSlug' => $recentPost->getSlug() ]) }}">
+                    <li style="list-style: none; font-size: 1.25em; line-height: 1.9em">
+                        <div class="d-flex" style="justify-content: space-between">
+                            {{ $recentPost->getTitle() }}
+
+                            <div style="width:5em; justify-content: space-between; white-space: nowrap; color:#BBB;">
+                                {{ $recentPost->getDateTime()->format('M j, Y') }}
+                            </div>
+                        </div>
+                    </li>
+
+                </a>
+            @endforeach
+
+            @include('_snippets/homepage_separator')
+
             <h2>
                 What do CTOs <span class="text-rector-green">Love&nbsp;about&nbsp;Rector</span>?
             </h2>
