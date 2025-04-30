8434d36dea00c7fb7f5721a8dc1861c35f0f686f
Tomas Votruba <tomas.vot@gmail.com>
2025-04-30 22:40:17 +0200

    Make menu js (#3008)
    * udpate demo click on, with simple css spinner
    
    * add working mobile + desktop menu
    
    * add dom reference
    
    * mobile menu

--- resources/views/hire_team/references.blade.php
+++ resources/views/hire_team/references.blade.php
@@ -1,6 +1,26 @@
 <div id="references">
-    <div class="row mb-5">
-        <div class="card shadow ms-2 mt-4">
+    <div class="row mb-5 ps-2 pe-2">
+        <div class="card shadow mt-3 pb-2">
+            <div class="card-body company-quote-card-body" style="display: flex">
+                <div>
+                    <img src="/assets/images/faces/dom_graziano.jpg" class="rounded-circle img-face-smaller-left me-4" alt="Dom Graziano face">
+                </div>
+
+                <blockquote class="blockquote company-quote me-4">
+                    <p>
+                        "In my time as Head of software engineering at i6, I found in Rector the perfect core and <strong>enabling team to tackle technical debt</strong>, unblock critical upgrades and build strong foundations for leading aviation software project."
+                    </p>
+                    <footer class="blockquote-footer mt-1">
+                        Dom Graziano, Head of Software Engineering at i6 Group
+                    </footer>
+                </blockquote>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row mb-5 ps-2 pe-2">
+        <div class="card shadow mt-3 pb-2">
             <div class="card-body company-quote-card-body" style="display: flex">
                 <div>
                     <img src="https://www.adamgleiss.com/images/face.jpg"
@@ -20,8 +40,55 @@
         </div>
     </div>
 
-    <div class="row mb-5">
-        <div class="card shadow ms-2 mt-4">
+    <div class="row mb-5 ps-2 pe-2">
+        <div class="card shadow mt-3 pb-2">
+            <div class="card-body company-quote-card-body" style="display: flex">
+                <div>
+                    <img src="/assets/images/faces/eric_molitor.jpg"
+                         class="rounded-circle img-face-smaller-left me-4" alt="Eric Molitor face">
+                </div>
+
+                <blockquote class="blockquote company-quote me-4">
+                    <p>
+                        "Tomas' deep expertise and tools-driven approach allowed us to modernize a significantly large legacy PHP codebase while teaching us best practices.
+                    </p>
+                    <p>
+                        Rector team <strong>worked around our schedule to ensure minimal disruption</strong>.
+                    </p>
+                    <p>
+                        I highly recommend Tomas and his team for legacy PHP code modernization!"
+                    </p>
+                    <footer class="blockquote-footer mt-1">
+                        Eric Molitor, CTO at Curve
+                    </footer>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mb-5 ps-2 pe-2">
+        <div class="card shadow mt-3 pb-2">
+            <div class="card-body company-quote-card-body" style="display: flex">
+                <div>
+                    <img src="/assets/images/faces/milan_mimra.jpg"
+                         class="rounded-circle img-face-smaller-left me-4" alt="Milan Mimra face">
+                </div>
+
+                <blockquote class="blockquote company-quote me-4">
+                    <p>
+                        "Thanks to Rector, we were able to quite simply refactor the core of our API, which <strong>saved a lot of work that our developers would otherwise have to do manually</strong>."
+
+                    </p>
+                    <footer class="blockquote-footer mt-1">
+                        Milan Mimra, CTO at Spaceflow
+                    </footer>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mb-5 ps-2 pe-2">
+        <div class="card shadow mt-3 pb-2">
             <div class="card-body company-quote-card-body" style="display: flex">
                 <div>
                     <img src="/assets/images/faces/nathan_page.jpg"
