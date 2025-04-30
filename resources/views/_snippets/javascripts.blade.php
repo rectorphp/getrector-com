0c6c86f717574f3667b8bc959acc08ef295db42e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-04-30 08:20:24 +0700

    Fix bootstrap menu not loaded on homepage /

--- resources/views/_snippets/javascripts.blade.php
+++ resources/views/_snippets/javascripts.blade.php
@@ -1,8 +1,7 @@
-@isset ($codeMirror)
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+@isset ($codeMirror)
 
 {{-- live code highligh in demo --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.28.0/codemirror.min.js"></script>
