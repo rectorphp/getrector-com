5a679d637de2054a5a01d07d4b251e48d4c93ec7
Tomas Votruba <tomas.vot@gmail.com>
2025-05-06 10:33:03 +0200

    fix menu item duplicates

--- resources/views/_snippets/menu_items.blade.php
+++ resources/views/_snippets/menu_items.blade.php
@@ -29,29 +29,13 @@
        class="nav-link">Contact</a>
 </li>
 
-
-
-
-@if (!isset($includeFooterLinks))
-    <li class="nav-item">
-        <a href="{{ action(\App\Ast\Controller\AstController::class) }}"
-           class="nav-link">AST</a>
-    </li>
-@endif
+<li class="nav-item">
+    <a href="{{ action(\App\Ast\Controller\AstController::class) }}"
+       class="nav-link">AST</a>
+</li>
 
 @isset ($includeFooterLinks)
     <li class="nav-item">
-        <a href="{{ action(\App\Ast\Controller\AstController::class) }}"
-           class="nav-link">Play with AST</a>
-    </li>
-
-    <li class="nav-item">
-        <a href="{{ action(\App\Controller\FindRuleController::class) }}"
-           class="nav-link">Find rule</a>
-    </li>
-
-    <li class="nav-item">
         <a onclick="toggleTheme()" class="nav-link" id="theme_toggle" style="cursor: pointer">Dark/Light Theme</a>
     </li>
-
 @endisset
