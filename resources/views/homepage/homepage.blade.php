63984c9b30686341eb53f50ff3ed8dcfd65843aa
Tomas Votruba <tomas.vot@gmail.com>
2025-05-09 11:45:15 +0200

    add more shortcuts

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -139,7 +139,6 @@
             @endif
 
             @include('homepage/_parts/posts')
-
             @include('_snippets/homepage_separator')
 
             <h2>
@@ -147,7 +146,6 @@
             </h2>
 
             @include('hire_team/references')
-
             @include('_snippets/homepage_separator')
 
             <h2>FAQ</h2>
@@ -155,15 +153,4 @@
             @include('homepage/_parts/faq')
         </div>
     </div>
-
-    <script>
-        // you've found an easter egg!
-
-        // press "f" to go to "/find-rule"
-        document.addEventListener('keydown', (event) => {
-            if ((event.key === 'f' || event.key === 'F') && !event.ctrlKey && !event.target.isContentEditable && event.target.tagName !== 'INPUT' && event.target.tagName !== 'TEXTAREA') {
-                window.location.href = '/find-rule';
-            }
-        });
-    </script>
 @endsection
