0d15c9487376813c8e400b0ca2f77ce7c8acf503
Jordan Welch <jordan@gi.insure>
2026-04-08 12:52:44 -0500

    Add `withoutVite` for testing frontend

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -11,6 +11,12 @@ use Override;
 
 abstract class AbstractTestCase extends TestCase
 {
+    public function setup(): void
+    {
+        parent::setup();
+        $this->withoutVite();
+    }
+
     #[Override]
     public function createApplication(): Application
     {
