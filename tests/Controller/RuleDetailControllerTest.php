abc7b24496ea514f202d2adfddc927c0b57c6cea
Jordan Welch <jordan@gi.insure>
2026-04-08 12:54:31 -0500

    backfill `RuleDetailControllerTest` test

---
+++ tests/Controller/RuleDetailControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Tests\AbstractTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RuleDetailControllerTest extends AbstractTestCase
+{
+    public function testItRendersRulesBySlug(): void
+    {
+        $response = $this->get('/rule-detail/simplify-array-search-rector');
+
+        $response
+            ->assertOk()
+            ->assertViewIs('homepage.rule_detail')
+            ->assertViewHas('ruleMetadata')
+            ->assertViewHas('codeMirror', true)
+            ->assertViewHas('page_title', 'SimplifyArraySearchRector')
+            ->assertSeeText('SimplifyArraySearchRector');
+    }
+
+    public function testItRedirectsToFindRuleIfRuleNotFound(): void
+    {
+        $response = $this->get('/rule-detail/unknown-rule');
+
+        $response->assertRedirect('/find-rule');
+    }
+}
